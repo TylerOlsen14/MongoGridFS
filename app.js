f51601cf113a46bd3561a92453ab0616daeccd40
TylerOlsen14 <Tyler.Olsen1@gmail.com>
2019-05-07 11:15:35 -0600

    Set up routes for upload, get, get/file, get/image

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const crypto = require('crypto'); // core node mondule
 const mongoose = require('mongoose')
 const multer = require('multer')
 const GridFsStorage = require('multer-gridfs-storage')
-const Grid = require('gridfs-storage')
+const Grid = require('gridfs-stream')
 const methodOverride = require('method-override')
 
 
@@ -34,10 +34,11 @@ conn.once('open', () => {
 // Create storage engine
 const storage = new GridFsStorage({
   url: mongoURI,
-  file: (req, file) => { // sets up a promise here
+  file: (req, file) => {
+    return new Promise((resolve, reject) => { // sets up a promise here
     crypto.randomBytes(16, (err, buf) => { // method that generates names for the DB
       if (err) {
-        return PromiseRejectionEvent(err);
+        return reject(err);
       }
       const filename = buf.toString('hex') + path.extname(file.originalname);
       const fileInfo = {
@@ -46,6 +47,7 @@ const storage = new GridFsStorage({
       };
       resolve(fileInfo)
     })
+  })
   }
 })
 const upload = multer({ storage }) //acts as the middleware for index.ejs 
@@ -59,12 +61,62 @@ app.get('/', (req, res) => {
 // @route POST /upload
 // @desc Uploads file to DB
 app.post('/upload', upload.single('file'), (req, res) => {// .single => meant to upload a single file
-  res.json({file: req.file})
+  // res.json({file: req.file})
+  res.redirect('/')
 })
 
+// @route GET /files
+// @desc display all files in JSON
+app.get('/files', (req, res) =>{
+  gfs.files.find().toArray((err, files) => {
+    // check to see if files exist
+    if(!files || files.length === 0){
+      return res.status(404).json({
+        error: "No files exist"
+      })
+    }
+    // Files do exist
+    return res.json(files);
+  })
+})
 
+// @route GET /files
+// @desc display single file object in JSON
+app.get('/files/:filename', (req, res) =>{
+  gfs.files.findOne({filename: req.params.filename}, (err, file) => {
+    // check to see if file exists
+    if(!file || file.length === 0){
+      return res.status(404).json({
+        error: "No files exist"
+      })
+    }
+    // File does exist
+    return res.json(file);
+  })
+})
 
-
+// @route GET /image/"filename"
+// @desc Display image
+app.get('/image/:filename', (req, res) =>{
+  gfs.files.findOne({filename: req.params.filename}, (err, file) => {
+    // check to see if file exists
+    if(!file || file.length === 0){
+      return res.status(404).json({
+        error: "No files exist"
+      })
+    }
+    // Check if image
+    if(file.contentType === 'image/jpeg' || file.contentType === 'img/png') {
+      // Read output to browser
+      const readstream = gfs.createReadStream(file.filename);
+        readstream.pipe(res)
+    } else {
+      res.status(404).json({
+        err: 'Not an image'
+      })
+    }
+  })
+})
 
 
 
